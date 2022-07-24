f740b17ccaa58756826d20723ca0c58fe72c0286
Ismael Gutierrez <ismaelgutierrezdev@gmail.com>
2022-07-24 13:58:08 -0600

    Papel y tijeras inicial

--- src/proyects/piedraPapel/Main.js
+++ src/proyects/piedraPapel/Main.js
@@ -1,126 +1,132 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import './style.css';
+
 import piedra from './img/piedra.png';
 import papel from './img/papel.png';
 import tijera from './img/tijeras.png';
-import interrogacion from './img/interrogacion.png';
 import piedrafull from './img/piedrafull.png';
 import papelfull from './img/papelfull.png';
 import tijerafull from './img/tijerafull.png';
+import interrogacion from './img/interrogacion.png';
 
-
-
-
-const Header = () => {
-    return(
-        <div className='header'>
-            <h1>Piedra, Papel y Tijera</h1>
-        </div>
-    );
-};
-
-const Puntajes = ({nombres, puntos}) => {
-    console.log('Render puntajes');
-    return(
-        <div className="puntaje">
-            <div>
-                {nombres[0]}: {puntos[0]} pts
-            </div>
-            <div>
-                <button>Reiniciar</button>
-            </div>
-            <div>
-                {nombres[1]}: {puntos[0]} pts
-            </div>
-        </div>
-    );
-};
-
-const Instrucciones = ({turno, nombres}) => {
-    return(
-        <div className='instruccion'>
-            <h2>Jugador {(turno)?nombres[0]:nombres[1]} es tu turno</h2>
-        </div>
-    );
-};
-
-const Seleccion = ({turno, setTurno, seleccion, setSeleccion}) => {
-    const [nombre, setNombre] = useState('...');
-
-    const mostrarNombre = (e) => {
-        setNombre(e.target.id);
-    };
-    const ocultarNombre = () => {
-        setNombre('...');
-    };
-
-    const hacerSeleccion = (e) => {
-        if(turno) {
-            setSeleccion([e.target.id, '']);
-        } else {
-            setSeleccion([seleccion[0], e.target.id]);
-        }
-        setTurno(!turno);
-    };
-    
-
-
-    return (
-        <div className="seleccion">
-            <h4>{nombre}</h4>
-            <img id="Piedra" src={piedra} alt="Foto de piedra" loading='lazy' width="60" height="60" onMouseEnter={mostrarNombre} onMouseLeave={ocultarNombre} onClick={hacerSeleccion} />
-
-            <img id="Papel" src={papel} alt="Foto de papel" loading='lazy' width="60" height="60" onMouseEnter={mostrarNombre} onMouseLeave={ocultarNombre} />
-
-            <img id="Tijera" src={tijera} alt="Foto de tijera" loading='lazy' width="60" height="60" onMouseEnter={mostrarNombre} onMouseLeave={ocultarNombre} />
-        </div>
-    );
-};
-
-const Versus = () => {
-    return (
-        <div className='versus'>
-            <img src={interrogacion} alt="Foto de piedra full" loading='lazy' width="250" />
-        </div>
-    );
-};
-
-const Escenario = ({turno, setTurno, seleccion, setSeleccion}) => {
-    return(
-        <div className='escenario'>
-            <div className='zona1'>
-                <Seleccion turno={turno} setTurno={setTurno} seleccion={seleccion} setSeleccion={setSeleccion} />
-                <Versus seleccion={seleccion} />
-            </div>
-            <div className='vs'>
-                <h2>VS</h2>
-            </div>
-            <div className='zona2'>
-                <Seleccion />
-                <Versus />
-            </div>
-        </div>
-    );
-};
 
 const Main = () => {
-    const [nombres, setNombres] = useState(['Jugador1', 'Computadora']);
+    const [nombreJugador, setNombreJugador] = useState('');
     const [puntos, setPuntos] = useState([0, 0]);
-    const [seleccion, setSeleccion] = useState([]);
-    let [turno, setTurno] = useState(true);
+    const [turno, setTurno] = useState(true);
+    const [seleccion, setSeleccion] = useState([null, null]);
+    const [resultados, setResultados] = useState('');
+    const items = [
+        {nombre: 'piedra', foto: piedra, fotofull: piedrafull},
+        {nombre: 'papel', foto: papel, fotofull: papelfull},
+        {nombre: 'tijera', foto: tijera, fotofull: tijerafull}
+    ];
 
-    // useEffect(() => {
-    //     let jugador = prompt('Ingresa tu nombre: ');
-    //     setNombres([jugador, 'Computadora']);
-    // }, []);
+    const hacerSeleccion = (e) => {
+        setTurno(!turno);
+        setSeleccion([e.target.id, null]);
 
+        let seleccionComputer = Math.floor(Math.random()*3);
+        setTimeout(() => {
+            setSeleccion([e.target.id, seleccionComputer.toString()]);
+            console.log('la computadora selecciono -> ' +  seleccionComputer);
+        }, 2000);
+    };
+
+    useEffect(() => {
+        setNombreJugador(prompt('Ingresa tu nombre:'));
+    }, []);
+
+    useEffect(() => {
+        console.log('calculando');
+        if(seleccion[0]!=null && seleccion[1] != null) {
+            const res = items[seleccion[0]].nombre + items[seleccion[1]].nombre;
+            if(res === 'piedrapiedra' || res === 'papelpapel' || res === 'tijeratijera') {
+                setResultados(['', 'Es un empate', items[seleccion[0]].fotofull]);
+            } else {
+                setTimeout(() => {
+                    if(res === 'piedratijera' || res === 'papelpiedra' || res === 'tijerapapel') {
+                        setResultados([nombreJugador, 'Has sido el ganador', items[seleccion[0]].fotofull]);
+                        setPuntos([puntos[0]+1, puntos[1]]);
+                    } else if(res === 'piedrapapel' || res === 'papeltijera' || res === 'tijerapiedra') {
+                        setResultados(['Computadora', 'Es la ganadora', items[seleccion[1]].fotofull]);
+                        setPuntos([puntos[0], puntos[1]+1]);
+                    }
+                }, 2000);
+            }
+        }
+    }, [seleccion]);
+
+    const continuar = () => {
+        setResultados(['']);
+        setSeleccion([null, null]);
+        setTurno(true);
+    };
+
+    const reiniciar = () => {
+        continuar();
+        setPuntos([0, 0]);
+        setNombreJugador(prompt('Ingresa tu nombre: '));
+    };
 
     return(
         <div className="contenedor">
-            <Header />
-            <Puntajes nombres={nombres} puntos={puntos}/>
-            <Instrucciones turno={turno} nombres={nombres} />
-            <Escenario turno={turno} setTurno={setTurno} seleccion={seleccion} setSeleccion={setSeleccion} />
+            <div className="header">
+                <h1>Piedra, Papel y Tijera</h1>
+            </div>
+
+            <div className='contenido'>
+                <div className='puntaje'>
+                    <div className='marcador'>
+                        <h3>{nombreJugador}: {puntos[0]} Pts</h3>
+                    </div>
+                    <div className='marcador'>
+                        <h3>Computadora: {puntos[1]} Pts</h3>
+                    </div>
+                </div>
+
+                <div className='instrucciones'>
+                    <h3>{(turno ? nombreJugador : 'Computadora')} es tú turno</h3>
+                </div>
+
+                <div className={`seleccion-item ${(!turno ? 'disabled' : '')}`}>
+                    {
+                        items.map((v, i) => {
+                            return <img key={i} id={i} src={v.foto} alt={`Foto de item ${v.nombre}`} loading="lazy" width={60} height={60} onClick={hacerSeleccion} />;
+                        })
+                    }
+                </div>
+
+                <div className='versus'>
+                    <div className='zona1'>
+                        <img src={(seleccion[0]==null ? interrogacion : items[seleccion[0]].fotofull)} alt="Texto alternativo" loading='lazy' width={250} height={250} />
+                    </div>
+                    <div className='vs'>
+                        <h3>VS</h3>
+                    </div>
+                    <div className='zona2'>
+                        <img src={(seleccion[1]==null ? interrogacion : items[seleccion[1]].fotofull)} alt="Texto alternativo" loading='lazy' width={250} height={250} />
+                        {console.log(seleccion)}
+                    </div>
+                </div>
+            </div>
+            <div className={`modal-resultados ${(resultados=='') ? 'ocultar' : 'mostrar-modal'}`}>
+                <div className='all'>
+                    <div className='head'>
+                        <h2>Resultado</h2>
+                    </div>
+                    <div className='body'>
+                        <h1>{resultados[0]}</h1>
+                        <h2>{resultados[1]}</h2>
+                        <img src={resultados[2]} alt="Foto del ganador" loading='lazy' width={200} height={200} />
+
+                        <div className='buttons'>
+                            <button onClick={continuar}>Continuar</button>
+                            <button onClick={reiniciar}>Reiniciar</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
